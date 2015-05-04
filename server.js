e9fc059f36eb7e294ae560d5d880ba26a92dc4fb
Ivan Reese <ivan@cdiginc.com>
2015-05-04 18:54:05 +0200

    Set the port, maybe?

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ var express = require('express'),
 // Everything in public will be accessible from '/'
 app.use(express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(process.env.PORT || 3000);
