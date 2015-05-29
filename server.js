9efafb5c4f78deb32c75e338d036e6688ab753e0
Ivan Reese <ivan@cdiginc.com>
2015-05-29 14:53:21 +0200

    Add gzip and caching to node server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    app = express();
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var maxAge = 1000 * 60 * 60 * 24;
+var app = express();
+
+app.use(express.compress());
 
 // Everything in public will be accessible from '/'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: maxAge }));
 
 http.createServer(app).listen(process.env.PORT || 3000);
